3bec35edb12df8a54b5b11e79c40baf5aa8ecfcf
jamechet <rabou.von@gmail.com>
2018-08-23 13:26:28 +1000

    Add MavenSpringBootSQL Project

---
+++ MavenSpringBootSQL/src/main/java/com/maven/springboot/MavenSpringBootSQL/MavenSpringBootSqlApplication.java
@@ -0,0 +1,12 @@
+package com.maven.springboot.MavenSpringBootSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringBootSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpringBootSqlApplication.class, args);
+	}
+}
